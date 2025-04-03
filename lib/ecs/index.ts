571241e97ca0d9c39c173c227ccdd645b79a0c03
Alex Thewsey <thewsey@amazon.com>
2025-04-03 17:25:41 +0800

    chore(ecs): drop unused import

--- lib/ecs/index.ts
+++ lib/ecs/index.ts
@@ -1,4 +1,4 @@
-import { Duration, Stack } from "aws-cdk-lib";
+import { Duration } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecs from "aws-cdk-lib/aws-ecs";
